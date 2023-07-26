b7403371912d037074494bad5746bbce22ec5ce4
ecmorgan360 <61012627+ecmorgan360@users.noreply.github.com>
2023-07-26 18:13:08 +0200

    Add files via upload

---
+++ trial.hs
@@ -0,0 +1,36 @@
+-- Not working at all yet
+
+-- Making a Text editor line type, with a string with all characters in the line, and a counter for how
+-- many characters can still be put on this line
+data TELine = TELine 
+    { contents :: String
+    , charspaceleft :: Integer
+    } deriving Show
+
+main :: IO()
+main = do
+    putStrLn ("Welcome to Micro. When you wish to exit, please press the escape key.")
+    let currentLine = TELine "" 120
+        in getnewInput currentLine
+    putStrLn ("Thank you for using Micro.")
+
+appendToLine :: TELine -> TELine
+appendToLine line =
+    let nextchar = getnewInput 
+        in
+            if (nextchar == '\ESC')
+                then line
+            else
+                --getnewInput (appendChar nextchar line)
+                appendToLine (appendChar nextchar line)
+
+getnewInput :: IO() -> Char
+getnewInput = do 
+    nextchar <- getChar
+    return nextchar
+
+appendChar :: Char -> TELine -> TELine
+appendChar nextchar line = TELine ((contents line) ++ (charToString nextchar)) ((charspaceleft line) - 1)
+
+charToString :: Char -> String
+charToString = (:[])
