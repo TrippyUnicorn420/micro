6959caf0bd7872ba7d5fab12e6a35813ee2e0a40
TrippyUncorn420 <smahayi31@gmail.com>
2023-07-27 11:27:05 +0200

    oh my god it works

--- nodata.hs
+++ nodata.hs
@@ -2,18 +2,20 @@ main :: IO()
 main = do
     putStrLn ("Welcome to Micro. When you wish to exit, please press the escape key.")
     let currentLine = ""
-        in print (pure appendToLine currentLine)
+        in appendToLine (pure currentLine) >>= print
     putStrLn ("Thank you for using Micro.")
 
 appendToLine :: IO String -> IO String
 appendToLine line =
-    let nextchar = getnewInput 
-        in
-            if (nextchar == pure '\ESC')
-                then line
+    line
+    >>= \line ->
+        getnewInput
+        >>= \nextchar ->
+            if (nextchar == '\ESC')
+                then pure line
             else
                 --getnewInput (appendChar nextchar line)
-                appendToLine (fmap appendChar nextchar line)
+                appendToLine (pure (appendChar nextchar line))
 
 getnewInput :: IO Char
 getnewInput = do 
